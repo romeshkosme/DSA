a33ab7588af1ea06dfb85e5a9f32cda76929d856
Romesh Kosme <romesh.30@gmail.com>
2022-05-21 00:41:27 +0530

    circularLinkedList: InProgress

---
+++ circularLinkedList.js
@@ -0,0 +1,11 @@
+function Node(value) {
+    this.value = value;
+    this.next = null;
+}
+
+function CircularLinkedList() {
+    this.head = null;
+    this.insert = function (value) {
+        
+    }
+}
