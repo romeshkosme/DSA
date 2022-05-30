91a0dffe7664af2412a3b86c41e56e66aba16561
Romesh Kosme <romesh.30@gmail.com>
2022-05-31 01:11:51 +0530

    insertAt:Done

--- circularLinkedList.js
+++ circularLinkedList.js
@@ -1,11 +1,86 @@
+// 1. Circular Singly Linked List
+// 2. Circular Doubly Linked List
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function CircularLinkedList() {
-    this.head = null;
-    this.insert = function (value) {
-        
+  this.head = null;
+  this.insert = function (value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.head.next = this.head;
+    } else {
+      let current = this.head;
+      if (current.next === this.head) {
+        current.next = node;
+        node.next = this.head;
+      } else {
+        while (current.next !== this.head) {
+          current = current.next;
+        }
+        current.next = node;
+        node.next = this.head;
+      }
     }
+  };
+  // insert At
+  this.insertAt = function (value, index) {
+    const node = new Node(value);
+    let current = this.head;
+    // if list is blank: DONE
+    // else
+    if (!current) {
+      this.head = node;
+      this.head.next = this.head;
+    } else {
+      let count = 0;
+      if (index === 0) {
+        do {
+          current = current.next;
+        } while(current.next !== this.head)
+        current.next = node;
+        node.next = this.head;
+        this.head = node;
+        return;
+      }
+      while (count < index) {
+        count++;
+        if (count === index) break;
+        current = current.next;
+      }
+      if (current.next !== this.head) {
+        console.log("1");
+        node.next = current.next;
+        current.next = node;
+      } else {
+        current.next = node;
+        node.next = this.head;
+      }
+    }
+  };
+  // remove At
+  // list
+  this.list = function () {
+    let current = this.head;
+    do {
+      console.log(current.value);
+      current = current.next;
+    } while (current !== this.head);
+  };
+  // get
 }
+
+(() => {
+  const circularLinkedList = new CircularLinkedList();
+  circularLinkedList.insert(10);
+  circularLinkedList.insert(20);
+  circularLinkedList.insert(30);
+  circularLinkedList.insert(40);
+  //   circularLinkedList.insert(50);
+  circularLinkedList.insertAt(45, 4);
+  // circularLinkedList.insertAt(20, 1);
+  circularLinkedList.list();
+})();
